21c0938c3417c0dd3089d3473a28783bee71f29a
iii <estrada.flsw@gmail.com>
2026-06-15 21:14:22 -0600

    feat – edit style details

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,13 +7,13 @@ export default function Header() {
         <header className="w-full flex items-center justify-between gap-3">
             <div className="flex items-center gap-4">
                 <div className="size-10 aspect-square rounded-full object-top overflow-hidden">
-                    <img src={me.src} alt={site.description} className="h-16.75 object-cover" />
+                    <img src={me.src} alt={site.description} className="h-17 md:h-16.75 object-cover" />
                 </div>
                 <div className="select-none">
                     <h1 className="font-i text-[1rem] tracking-tighter text-black dark:text-white">
                         <span className="font-sans text-[1.05rem]">A</span>tlas Estrada
                     </h1>
-                    <h2 className="text-muted-foreground">Web Developer</h2>
+                    <h2 className="text-muted-foreground">Web developer</h2>
                 </div>
             </div>
             <Theme />
