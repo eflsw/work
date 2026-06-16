70263aa645fc7fe09ff260769137e7568c73ce97
iii <estrada.flsw@gmail.com>
2026-06-15 22:22:30 -0600

    feat – update section contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,32 +1,34 @@
 import { mail, gh, ig, x } from '@/data'
 import { Button } from './ui/button'
+import { IconArrowUpRight } from '@tabler/icons-react'
 
 export default function Contact() {
     return (
-        <p>
-            You can contact me via{' '}
-            <Button asChild variant="link">
-                <a href={mail.url}>{mail.title}</a>
-            </Button>
-            ,{' '}
-            <Button asChild variant="link">
-                <a href={x.url} target="_blank">
-                    {x.title}
+        <div className="w-full grid grid-cols-4 gap-2">
+            <Button asChild variant="outline" className="w-full">
+                <a href={mail.url}>
+                    Contact
+                    <IconArrowUpRight />
                 </a>
             </Button>
-            ,{' '}
-            <Button asChild variant="link">
-                <a href={ig.url} target="_blank">
-                    {ig.title}
-                </a>
-            </Button>
-            , or{' '}
-            <Button asChild variant="link">
+            <Button asChild variant="outline" className="w-full">
                 <a href={gh.url} target="_blank">
                     {gh.title}
+                    <IconArrowUpRight />
                 </a>
             </Button>
-            .
-        </p>
+            <Button asChild variant="outline" className="w-full">
+                <a href={ig.url} target="_blank">
+                    {ig.title}
+                    <IconArrowUpRight />
+                </a>
+            </Button>
+            <Button asChild variant="outline" className="w-full">
+                <a href={x.url} target="_blank">
+                    {x.title}
+                    <IconArrowUpRight />
+                </a>
+            </Button>
+        </div>
     )
 }
